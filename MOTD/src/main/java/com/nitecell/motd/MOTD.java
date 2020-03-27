5c65998a27bf9e3b7a773a713f859dc931b05479
Nitecell <62725636+Nitecell@users.noreply.github.com>
2020-03-27 16:45:46 -0700

    Added Configuration Support to MOTD
    Added Configuration Support to MOTD

--- MOTD/src/main/java/com/nitecell/motd/MOTD.java
+++ MOTD/src/main/java/com/nitecell/motd/MOTD.java
@@ -2,6 +2,8 @@ package com.nitecell.motd;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,8 +14,10 @@ public final class MOTD extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        System.out.println(ChatColor.GREEN + "[MOTD] has been enabled.");
+        getConfig().options().copyDefaults(true);
+        saveConfig();
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        System.out.println(ChatColor.GREEN + "[MOTD] has been enabled.");
     }
 
     @Override
@@ -24,6 +28,29 @@ public final class MOTD extends JavaPlugin implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        player.sendMessage(ChatColor.GREEN + "Welcome to the Test Server!");
+        player.sendMessage(ChatColor.GREEN + getConfig().getString("message"));
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("motd")) {
+            sender.sendMessage(getConfig().getString("message"));
+            return true;
+        }
+        if (cmd.getName().equalsIgnoreCase("setmotd")) {
+            if (args.length == 0) {
+                sender.sendMessage(ChatColor.RED + "Not enough arguments.");
+                return true;
+            }
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                str.append(args[i] + " ");
+            }
+            String motd = str.toString();
+            getConfig().set("message", motd);
+            saveConfig();
+            sender.sendMessage(ChatColor.GREEN + "New MOTD saved to the configuration file.");
+            return true;
+        }
+        return false;
     }
 }
